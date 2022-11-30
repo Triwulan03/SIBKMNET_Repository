82e53181abdd62ff162708b084d331563f4df343
Triwulan03 <triwulandari.tw71@gmail.com>
2022-11-30 16:29:19 +0700

    SIBKMNet

---
+++ SIBKMNET_WebApp/Context/MyContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SIBKMNET_WebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SIBKMNET_WebApp.Context
+{
+    public class MyContext : DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> dbContext) : base(dbContext)
+        {
+
+        }
+
+        public DbSet<Province> Provinces { get; set; }
+        public DbSet<Region> Regions { get; set; }
+            
+    }
+}
---
+++ SIBKMNET_WebApp/Contrtoller/ProvinceController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SIBKMNET_WebApp.Context;
+using SIBKMNET_WebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SIBKMNET_WebApp.Contrtoller
+{
+    public class ProvinceController : Controller
+    {
+        MyContext myContext;
+
+        public ProvinceController(MyContext myContext)
+        {
+            this.myContext = myContext;
+        }
+        public IActionResult Index()
+        {
+            var data = myContext.Provinces.Include(x => x.Region).ToList();
+            return View(data);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var data = myContext.Provinces.Include(x => x.Region).FirstOrDefault(x => x.Id.Equals(id));
+            return View(data);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Province country)
+        {
+            if (ModelState.IsValid)
+            {
+                myContext.Provinces.Add(country);
+               var result = myContext.SaveChanges();
+                if (result > 0)
+                    return RedirectToAction("Index"); 
+
+            }
+            return View();
+        }
+    }
+}
